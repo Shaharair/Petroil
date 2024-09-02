fd6fdf7c4cc4a7c82142c398147d7cd1cd02709d
Shahariar <shahariarmd806@gmail.com>
2024-09-02 19:02:17 +0600

    demo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -18,14 +18,14 @@ const Navbar = () => {
                 <img src={LogoImg} alt="logo" />
             </div>
             <div className="">
-                <ul className={`lg:flex gap-5 absolute z-[50] top-0 left-0 lg:static duration-700 ease-in-out ${show == true ? "bg-[gray] top-[200px] w-full" : " left-[-400px] w-full"}`}>
+                <ul className={`lg:flex gap-5 absolute z-[50] top-0 left-0 pt-[40px] lg:pt-0 lg:static duration-700 ease-in-out ${show == true ? "bg-[gray] top-[220px] w-[200px] h-[300px]" : " left-[-400px]"}`}>
                     <li className="font-sans font-semibold text-[18px] text-center lg:text-start text-[white] pt-[10px] lg:pt-0">Home</li>
                     <li className="font-sans font-semibold text-[18px] text-center lg:text-start text-[white]">About</li>
                     <li className="font-sans font-semibold text-[18px] text-center lg:text-start text-[white] py-1 lg:py-0">Services</li>
                     <li className="font-sans font-semibold text-[18px] text-center lg:text-start text-[white]">Gallery</li>
                     <li className="font-sans font-semibold text-[18px] text-center lg:text-start text-[white] py-1 lg:py-0">Blog</li>
-                    <div className="lg:pl-[50px] pt-5 lg:pt-0 py-7 lg:py-0 pl-[110px]">
-                        <a href="$" className="font-sans font-semibold text-[18px] text-[white] border-2 border-[white] px-[30px] py-3 uppercase hover:bg-[white] hover:text-[black] rounded duration-300 ease-in-out">Contact</a>
+                    <div className="lg:pl-[50px] pt-5 lg:pt-0 py-7 lg:py-0 pl-[50px]">
+                        <a href="$" className="font-sans font-semibold lg:text-[18px] text-[14px] text-[white] border-2 border-[white] lg:px-[30px] px-[20px] lg:py-3 py-2 uppercase hover:bg-[white] hover:text-[black] rounded duration-300 ease-in-out">Contact</a>
                     </div>
                 </ul>
             </div>
